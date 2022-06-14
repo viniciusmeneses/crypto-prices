3aac326d28e58417bba5b1ca0519870200d8919b
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-06-14 11:23:06 -0300

    chore: setup apollo and graphql codegen

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
+import { ApolloProvider } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import { apolloClient } from "./graphql";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
 );
