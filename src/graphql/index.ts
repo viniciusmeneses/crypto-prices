fda5ab3fcd4924198b1ec6ef0b7f36bd6699f43b
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-06-15 20:00:25 -0300

    feat: do API queries in batch

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,6 +1,10 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { BatchHttpLink } from "@apollo/client/link/batch-http";
+
+const link = new HttpLink({ uri: process.env.REACT_APP_API_URL });
+const batchLink = new BatchHttpLink({ uri: process.env.REACT_APP_API_URL });
 
 export const apolloClient = new ApolloClient({
-  uri: process.env.REACT_APP_API_URL,
+  link: process.env.NODE_ENV === "test" ? link : batchLink,
   cache: new InMemoryCache(),
 });
