2708316bd230ae81e1e28becbe5a8e2ef7a507b6
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-06-16 10:47:28 -0300

    fix: remove batch http link

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,10 +1,6 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
-import { BatchHttpLink } from "@apollo/client/link/batch-http";
-
-const link = new HttpLink({ uri: process.env.REACT_APP_API_URL });
-const batchLink = new BatchHttpLink({ uri: process.env.REACT_APP_API_URL });
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-  link: process.env.NODE_ENV === "test" ? link : batchLink,
+  uri: process.env.REACT_APP_API_URL,
   cache: new InMemoryCache(),
 });
