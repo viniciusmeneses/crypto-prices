67a7dacf38650c02cab6d2b47fd04c79cfe8cdfb
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-06-16 10:49:50 -0300

    feat: add new colors to theme

--- src/Theme.tsx
+++ src/Theme.tsx
@@ -10,9 +10,10 @@ export const theme = {
   colors: {
     primary: "#fd4b24",
     white: "#fff",
-    gray: "#cbced1",
+    gray: "#b1b1b1",
     black: "#000",
     error: "#ff3838",
+    whiteTransparent: "#ffffff80",
   },
 };
 
