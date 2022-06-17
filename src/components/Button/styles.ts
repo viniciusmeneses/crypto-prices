59365609071d9ff7915e0fda804956612c177be1
Vinicius Meneses <vinicius.meneses04@gmail.com>
2022-06-17 00:34:05 -0300

    fix: adjust Button styles

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -7,15 +7,20 @@ export const StyledButton = styled.button<ButtonProps>`
   background-color: ${({ theme }) => theme.colors.primary};
   color: #fff;
   padding: 0 2rem;
-  border-radius: 2rem;
-  min-height: 2rem;
+  border-radius: 1.5rem;
+  min-height: 2.75rem;
   border: none;
   font-size: 1rem;
   width: ${({ fullWidth }) => (fullWidth ? "100%" : "auto")};
   opacity: ${({ disabled, loading }) => (disabled || loading ? 0.5 : 1)};
   transition: ease 0.2s;
+  cursor: pointer;
 
   &:hover {
-    background-color: ${({ theme }) => darken(0.2, theme.colors.primary)};
+    background-color: ${({ theme }) => darken(0.1, theme.colors.primary)};
+  }
+
+  &:disabled {
+    cursor: default;
   }
 `;
